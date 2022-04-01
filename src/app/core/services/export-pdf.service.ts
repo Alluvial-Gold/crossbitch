2e5b8aa9b17ccfdcbf4254aded569f01d6a77545
Alluvial-Gold <74955687+Alluvial-Gold@users.noreply.github.com>
2022-04-02 00:46:49 +1030

    Added icons and grid numbers to pdf export

--- src/app/core/services/export-pdf.service.ts
+++ src/app/core/services/export-pdf.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ProjectModel } from '../state/project.model';
-import { PDFDocument, StandardFonts, rgb, PDFPage, grayscale, RGB } from 'pdf-lib'
+import { PDFDocument, StandardFonts, rgb, PDFPage, grayscale, RGB, RotationTypes } from 'pdf-lib'
 import { BasicLayer } from '../state/basic-layer.model';
+import { Icons } from 'src/app/shared/icons.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,7 @@ export class ExportPdfService {
     const page = pdfDoc.addPage()
     await this.createFrontPage(project, page, timesRomanFont, timesRomanBoldFont);
 
-    this.drawPattern(project, pdfDoc);
+    this.drawPattern(project, pdfDoc, timesRomanFont);
 
     const pdfBytes = await pdfDoc.save()
     this.export2(pdfBytes);
@@ -42,7 +43,7 @@ export class ExportPdfService {
 
     // Project name
     let titleFontSize = 30;
-    page.drawText('[TODO: Project Name]', {
+    page.drawText(project.name, {
       x: 50,
       y: height - 2 * titleFontSize,
       size: titleFontSize,
@@ -88,13 +89,26 @@ export class ExportPdfService {
       let colour = this.hexToRgb(paletteEntry.floss.colour);
       page.drawRectangle({
         x: 50,
-        y: currentY - 2,
-        width: 10,
-        height: 10,
+        y: currentY - 3,
+        width: 12,
+        height: 12,
         borderWidth: 1,
         borderColor: rgb(0, 0, 0),
-        color: colour ? rgb( colour.r, colour.g, colour.b) : rgb(0, 0, 0),
+        //color: colour ? rgb( colour.r, colour.g, colour.b) : rgb(0, 0, 0),
+        color: rgb(1, 1, 1),
       })
+
+      // Draw symbol..
+      let icons = Icons;
+      page.drawSvgPath(
+        icons[paletteEntry.iconIndex].path,
+        {
+          x: 50 + 1,
+          y: currentY + 8,
+          scale: 0.1,
+          color: rgb(0, 0, 0)
+        }
+      );
 
       // Strands
       page.drawText(`[${paletteEntry.strands}]`, {
@@ -107,9 +121,9 @@ export class ExportPdfService {
       });
 
       // Stitches
-      page.drawText(`X stitches`, {
-        x: 250, 
-      });
+      // page.drawText(`X stitches`, {
+      //   x: 250, 
+      // });
 
       page.moveDown(20);
     }
@@ -127,7 +141,7 @@ export class ExportPdfService {
     } : null;
   }
 
-  private drawPattern(project: ProjectModel, pdfDoc: PDFDocument) {
+  private drawPattern(project: ProjectModel, pdfDoc: PDFDocument, font: any) {
     // TODO - split across pages...
     let page = pdfDoc.addPage();
     const { width, height } = page.getSize();
@@ -142,6 +156,14 @@ export class ExportPdfService {
       let colour = this.hexToRgb(p.floss.colour);
       let rgbColour: RGB = colour ? rgb( colour.r, colour.g, colour.b) : rgb(0, 0, 0);
       colourMap.set(i, rgbColour);
+    });
+
+    // palette icon map
+    let icons = Icons;
+    let iconMap = new Map<number, string>();
+    project.palette.forEach((p, i) => {
+      let iconPath = icons[p.iconIndex].path;
+      iconMap.set(i, iconPath);
     })
 
     let layer = project.layers[0];
@@ -153,13 +175,25 @@ export class ExportPdfService {
 
           if (typeof(val) == 'number' && val != -1) {
             let rgbColour = colourMap.get(val) ?? rgb(0, 0, 0);
-            page.drawRectangle({
-              x: startX + (rowIdx * squareSize ),
-              y: startY - ((colIdx + 1) * squareSize), // x, y positions...?
-              width: squareSize,
-              height: squareSize,
-              color: rgbColour
-            })
+            // page.drawRectangle({
+            //   x: startX + (rowIdx * squareSize ),
+            //   y: startY - ((colIdx + 1) * squareSize), // x, y positions...?
+            //   width: squareSize,
+            //   height: squareSize,
+            //   color: rgbColour
+            // })
+
+            // Draw symbol..
+            let iconPath = iconMap.get(val) ?? icons[0].path;
+            page.drawSvgPath(
+              iconPath,
+              {
+                x: startX + (rowIdx  * squareSize) + 1,
+                y: startY - (colIdx * squareSize) - 1,
+                scale: 0.08,
+                color: rgb(0, 0, 0)
+              }
+            );
           }
           // TODO partial stitches
         }
@@ -169,7 +203,7 @@ export class ExportPdfService {
 
     }
 
-    // TODO - grid
+    // grid
     let gridWidth = project.canvasSettings.columns * squareSize;
     let gridHeight = project.canvasSettings.rows * squareSize;
 
@@ -183,7 +217,8 @@ export class ExportPdfService {
           x: startX + x * squareSize,
           y: startY - gridHeight
         },
-        thickness: x % 10 == 0 ? 2 : 1
+        thickness: 1,
+        opacity: x % 10 == 0 ? 1 : 0.2
       })
     }
 
@@ -197,9 +232,51 @@ export class ExportPdfService {
           x: startX + gridWidth,
           y: startY - y * squareSize
         },
-        thickness: y % 10 == 0 ? 2 : 1
+        thickness: 1,
+        opacity: y % 10 == 0 ? 1 : 0.2
       })
     }
+
+    // numbers
+    let numberSize = 10;
+    for (let x = 10; x < project.canvasSettings.columns + 1; x += 10) {
+      page.drawText(x.toString(), {
+        x: startX + x * squareSize - 5,
+        y: startY + 5,
+        font: font,
+        size: numberSize
+      });
+    }
+
+    for (let y = 10; y < project.canvasSettings.rows + 1; y += 10) {
+      page.drawText(y.toString(), {
+        x: startX - 5,
+        y: startY - y * squareSize - 5,
+        font: font,
+        size: numberSize,
+        rotate: {
+          angle: 90,
+          type: RotationTypes.Degrees
+        }
+      });
+    }
+
+    // center arrows
+    page.drawSvgPath('M 0 0 L 10 0 L 5 20 Z', {
+      x: startX + gridWidth / 2 - 5,
+      y: startY + 20,
+      color: rgb(0, 0, 0),
+      opacity: 0.2
+    })
+
+    page.drawSvgPath('M 0 0 L 0 10 L 20 5 Z', {
+      x: startX - 20,
+      y: startY - gridHeight / 2 + 5,
+      color: rgb(0, 0, 0),
+      opacity: 0.2
+    })
+
+    // TODO - smaller key
   }
 
 }
